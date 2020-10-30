2df01611fdb1f261111df8f6a52c3dae0539eaea
hua-x <huaxineva@gmail.com>
2020-10-30 21:36:46 +0800

    first commit

---
+++ ZigZagConversion/ZigZagConversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        s_len = len(s)
+        if numRows == 1:
+            return s
+        
+        grp_size = 2 * numRows - 2
+        rows = ['' for _ in range(numRows)]
+        groups = [s[i:i+grp_size] for i in range(0, s_len, grp_size)]
+        for grp in groups:
+            for j in range(len(grp)):
+                if j < numRows:
+                    rows[j] += grp[j]
+                else:
+                    rows[grp_size-j] += grp[j] 
+        
+        result = ''
+        for row in rows:
+            result += row
+        
+        return result
