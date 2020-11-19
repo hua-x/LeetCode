b6b9c86ac4c38c30ce3e741b8ffe077f86812eac
huax-ubuntu <huaxineva@gmail.com>
2020-11-19 17:04:10 +0800

    add another approach where // to get first digit, and % to get last digit

--- Palindrome_2/test_Palindrome_wo_str.py
+++ Palindrome_2/test_Palindrome_wo_str.py
@@ -21,6 +21,28 @@ def isPalindrome(x: int) -> bool:
             
         return tmp == reverse_num, reverse_num
 
+
+
+def isPalindrome_another_approach(x):
+        if x < 0:
+            return False
+
+        ranger = 1
+        while x // ranger >= 10:
+            ranger *= 10
+
+        while x:
+            left = x // ranger
+            right = x % 10
+            if left != right:
+                return False
+            
+            x = (x % ranger) // 10
+            ranger //= 100
+
+        return True
+
+
 def print_result(s):
 	print(s)
 	result, num = isPalindrome(s) 	
