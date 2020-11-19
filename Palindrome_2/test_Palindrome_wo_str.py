db4eec38b7ffb9bd013b09bbf198c569c2301bf0
huax-ubuntu <huaxineva@gmail.com>
2020-11-19 16:39:13 +0800

    add Palindrome test without string func

---
+++ Palindrome_2/test_Palindrome_wo_str.py
@@ -0,0 +1,36 @@
+import math
+
+def isPalindrome(x: int) -> bool:
+
+        if x < 0:
+            return False, x
+        if x < 10:
+            return True, x
+
+        tmp = x
+        reverse_num = 0
+        length = int(math.log10(tmp))+1
+        count = length // 2
+
+        for i in range(count):
+            reverse_num += tmp % 10 * 10**(count - i -1)
+            tmp = tmp//10
+
+        if length % 2 != 0:
+            tmp = tmp // 10
+            
+        return tmp == reverse_num, reverse_num
+
+def print_result(s):
+	print(s)
+	result, num = isPalindrome(s) 	
+	print(result)
+	print(num)
+
+if __name__ == '__main__':
+	print_result(121)
+	print_result(-121)
+	print_result(10)
+	print_result(-101)
+	print_result(0)
+
